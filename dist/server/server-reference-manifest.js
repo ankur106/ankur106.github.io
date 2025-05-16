7c59840000a9b3d2f2902b762adeb299bdbab688
Ankur Patel <ankurnpatel106@gmail.com>
2025-05-16 00:58:39 -0700

    commit

--- dist/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"dunUGXQetaWCa5UAbwRygLz7Vz3GR8xWE1UQqxqX3Ec=\"}"
--- next.config.js
+++ next.config.js
@@ -1,20 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '',
+  assetPrefix: '',
   images: {
     unoptimized: true,
     domains: ['localhost'],
   },
-  basePath: '',
-  assetPrefix: '',
   webpack(config) {
     config.resolve.fallback = {
       fs: false,
-      net: false,
-      tls: false,
     };
     return config;
   },
+  trailingSlash: true,
+  experimental: {
+    outputStandalone: true,
+  },
+  generateStaticParams: true,
 }
 
 module.exports = nextConfig
