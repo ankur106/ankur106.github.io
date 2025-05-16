6c5dcef6f0c3e72eb3975377738e71568ad22daf
Ankur Patel <ankurnpatel106@gmail.com>
2025-05-16 01:07:55 -0700

    Update deployment configuration

--- next.config.js
+++ next.config.js
@@ -1,20 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: 'standalone',
+  basePath: '',
+  assetPrefix: '',
   images: {
     unoptimized: true,
     domains: ['localhost'],
   },
-  basePath: '',
-  assetPrefix: '',
   webpack(config) {
     config.resolve.fallback = {
       fs: false,
-      net: false,
-      tls: false,
     };
     return config;
   },
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
